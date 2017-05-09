2a9b0b82d9696e37de2a2889898c7b5e31882636
DESKTOP-ME3MOAG\Mukul <mukul.bajpai0821@gmail.com>
2017-05-09 16:03:39 -0700

    Cancer-detection changes and template change

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
 #this code uses an ensemble of different algorithms
+# here we will import the libraries used for machine learning
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv), data manipulation as in SQL
+import matplotlib
+from sklearn import model_selection
+import matplotlib.pyplot as plt # this is used for the plot the graph 
+import seaborn as sns # used for plot interactive graph. I like it most for plot
+from sklearn.linear_model import LogisticRegression # to apply the Logistic regression
+from sklearn.model_selection import train_test_split # to split the data into two parts
+from sklearn.cross_validation import KFold # use for cross validation
+from sklearn.model_selection import GridSearchCV# for tuning parameter
+from sklearn.ensemble import RandomForestClassifier # for random forest classifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
 import re
 import numpy as np
 import pandas as pd
@@ -32,6 +48,10 @@ def main():
 @app.route("/diabetes")
 def diabetesInput():
     return render_template('diabetes.html')
+	
+@app.route("/cancer")
+def cancerInput():
+    return render_template('cancer.html')
 
 @app.route('/diabetesPredictionPage',methods=['POST'])
 def diabetesClassifier():
@@ -62,9 +82,9 @@ def diabetesClassifier():
     y_pred = diab_dt.predict(X_test)
     output= int(y_pred[0])
     if output == 0:
-        return render_template('diabetesPrediction.html',value ="no diabetes")
+        return render_template('diabetesPrediction.html',value ="Congratulations!! You are safe from diabetes")
     else:
-        return render_template('diabetesPrediction.html',value ="diabetes")
+        return render_template('diabetesPrediction.html',value ="There are high chances of having diabetes. Please visit nearby hospital soon.")
 
 @app.route("/heartDisease")
 def heartDiseaseInput():
@@ -95,10 +115,85 @@ def heartDiseaseClassifier():
     output= int(y_pred[0])
 
     if output == 0:
-        return render_template('heartDiseasePrediction.html',value ="no heart disease")
+        return render_template('heartDiseasePrediction.html',value ="Congratulations!! You are safe from heart disease")
     else:
-        return render_template('heartDiseasePrediction.html',value ="Heart Disease")
+        return render_template('diabetesPrediction.html',value ="There are high chances of having heart disease. Please visit nearby hospital soon.")
 
+
+@app.route('/cancerPredictionPage',methods=['POST'])
+def cancerClassifier():
+
+	# Any results you write to the current directory are saved as output.
+	data = pd.read_csv("data.csv",header=0)# here header 0 means the 0 th row is our coloumn 
+	# have a look at the data
+	print(data.head(2))#
+	data.drop("Unnamed: 32",axis=1,inplace=True)
+	print data.columns
+	# like this we also don't want the Id column for our analysis
+	data.drop("id",axis=1,inplace=True)
+
+	features_mean= list(data.columns[1:11])
+	features_se= list(data.columns[11:20])
+	features_worst=list(data.columns[21:31])
+
+	data['diagnosis']=data['diagnosis'].map({'M':1,'B':0})
+	data.describe()
+	#print data[0: 10]
+	sns.countplot(data['diagnosis'],label="Count")
+	corr = data[features_mean].corr() # .corr is used for find corelation
+
+
+			   
+	#prediction_var = ['texture_mean','perimeter_mean','smoothness_mean','compactness_mean','symmetry_mean']
+	#prediction_var = features_mean
+	#low acuracy|||above both have low accuracy
+
+	prediction_var = features_worst
+	# the accuracy for RandomForest invcrease it means the value are more catogrical in Worst part
+	#lets get the important features
+	model=RandomForestClassifier(n_estimators=100,criterion="entropy")
+
+
+
+	#featimp = pd.Series(model.feature_importances_, index=prediction_var).sort_values(ascending=False)
+	#print(featimp)
+	# this is the property of Random Forest classifier that it provide us the importance 
+	# of the features used
+
+	#prediction_var = ['concave points_worst','radius_worst','area_worst','perimeter_worst','concavity_worst'] 
+	prediction_var = ['concave points_worst','radius_worst','area_worst','perimeter_worst','concavity_worst'] 
+
+	train, test = train_test_split(data, test_size = 0.3)
+
+	#RANDOMFORREST
+	train_X = train[prediction_var]# taking the training data input 
+	train_Y = train.diagnosis# This is output of our training data
+	# same we have to do for test
+	test_X= test[prediction_var] # taking test data inputs
+	test_Y =test.diagnosis   #output value of test dat
+	#model=RandomForestClassifier(n_estimators=100, criterion="entropy")
+	model.fit(train_X,train_Y)
+	print("------------------------------------")
+	# read the posted values from the UI
+	_concave_points_worst = request.form['concave points_worst']
+	_radius_worst = request.form['radius_worst']
+	_area_worst = request.form['area_worst']
+	_perimeter_worst = request.form['perimeter_worst']
+	_concavity_worst = request.form['concavity_worst']
+
+
+	print "inputs"
+	print _concave_points_worst, _radius_worst,_area_worst, _perimeter_worst, _concavity_worst
+	X_test=[_concave_points_worst, _radius_worst,_area_worst, _perimeter_worst, _concavity_worst]
+
+	prediction=model.predict(X_test)
+	print prediction
+	#print metrics.accuracy_score(prediction,test_Y)
+	output= int(prediction[0])
+	if output == 0:
+		return render_template('cancerPrediction.html',value ="Congratulations!! You are safe from cancer  ")
+	else:
+		return render_template('cancerPrediction.html',value ="There are high chances of having cancer. Please visit nearby hospital soon.")
 
 if __name__ == "__main__":
     app.run()
